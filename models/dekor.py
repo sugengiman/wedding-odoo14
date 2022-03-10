666b2ae38dee3edff61c141980d45fb2002484f6
sugengiman <sugengiman130901@gmail.com>
2022-03-10 11:21:09 +0700

    Created dekor.py  and patner.py

--- models/dekor.py
+++ models/dekor.py
@@ -1,9 +1,14 @@
-from multiprocessing.reduction import steal_handle
 from odoo import api, fields, models
 
 
-class ModuleName(models.Model):
-    _name = 'module.name'
-    _description = 'New Description'
+class Dekor(models.Model):
+    _name = 'wedding.dekor' #nama tabel dan nama model
+    _description = 'Model untuk tabel dekor'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Nama Dekorasi')
+    harga = fields.Integer(string='Harga Dekorasi') #ofint
+    deskripsi = fields.Char(string='Deskripsi Dekorasi') #ofchar
+
+    
+
+    
